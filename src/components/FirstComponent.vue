3480a1b8fbbe43d00a427c481a08c40a291b5a2f
htanzeel01 <54932095+htanzeel01@users.noreply.github.com>
2022-11-23 14:18:12 +0100

    firsvue

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+data(){
+    return{
+     items: [{
+        stock: 'hellow',
+        number: 25
+     },
+     {
+        stock: 'mellow',
+        number: 25
+    }
+]
+  }
+}
+}
+</script>
+<template>
+    <WelcomeItem>
+      <template #icon>
+      </template>
+      <template #heading>Workshop 1 is happeningng</template>
+          <table>
+            <li v-for="item in items">
+             {{ item.stock }} {{ item.number }}
+            </li>
+          </table>
+    </WelcomeItem>
+    </template>
+       
